0306ae5b59465558380ecfa072d7ee2d55a435ae
Dona-A <u.dona@yahoo.com>
2023-10-06 13:46:39 +0300

    m

--- src/Exercitiu1/Exercitiu1.java
+++ src/Exercitiu1/Exercitiu1.java
@@ -1,2 +1,27 @@
-package Exercitiu1;public class Exercitiu1 {
+package Exercitiu1;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Exercitiu1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("src/Exercitiu1/judete_in.txt"));
+        int i = 0;
+        String judete[] = new String[100];
+
+        while(scanner.hasNext()){
+            judete[i] = scanner.next();
+            i++;
+        }
+
+        System.out.println("Judete inainte de sortare:");
+        for(int j = 0 ; j < i ; j++)
+            System.out.println(judete[j]);
+
+        Arrays.sort(judete, 0, i);
+
+        System.out.println("Judete dupa sortare:");
+        for(int j = 0 ; j < i ; j++)
+            System.out.println(judete[j]);
+    }
 }
--- src/Exercitiu2/Exercitiu2.java
+++ src/Exercitiu2/Exercitiu2.java
@@ -1,2 +1,19 @@
-package Exercitiu2;public class Exercitiu2 {
+package Exercitiu2;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Exercitiu2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("src/Exercitiu2/cantec_in.txt"));
+        PrintStream flux_out = new PrintStream("cantec_out.txt");
+
+        while(scanner.hasNextLine()) {
+            String V = scanner.nextLine();
+            Vers versuri = new Vers(V);
+            flux_out.print(versuri.getVers());
+            flux_out.println(versuri.numarCuv());
+        }
+    }
 }
